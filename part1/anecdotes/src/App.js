e59049c5edbd6ead62916e3bcca0173ca6f10f84
Edenlia <zsy010906@gmail.com>
2022-01-17 17:44:20 +0800

    finish anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const GreateAnec = ({anecdotes, points}) => {
+  let index = 0
+  // console.log(points.length)
+  for (let i = 0; i < points.length; i++) {
+    if (points[index] < points[i]) index = i
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{anecdotes[index]}</div>
+      <div>has {points[index]} votes</div>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(anecdotes.map((value, index) => 0))
+
+  const randomAnec = () => {
+    const num = Math.floor(Math.random() * anecdotes.length)
+    setSelected(num)
+  }
+
+  const vote = (index) => {
+      // points[index] = points[index] + 1
+      const copy = [...points]
+      copy[index] += 1
+      setPoints(copy)
+  }
+
+  return (
+      <div>
+        <h1>Anecdote of the day</h1>
+        <div>{anecdotes[selected]}</div>
+        <div>has {points[selected]} votes</div>
+        <button onClick={() => vote(selected)}>vote</button>
+        <button onClick={() => randomAnec()}>next anecdote</button>
+        <h1>Anecdote with most votes</h1>
+        <GreateAnec points={points} anecdotes={anecdotes}/>
+      </div>
+
+
+  )
+}
+
+export default App
